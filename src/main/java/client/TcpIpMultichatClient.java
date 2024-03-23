279d9f0b72297d4d4c9ed61728fb2aac65ec6b73
gumgu <bienlee@naver.com>
2024-03-24 01:04:08 +0900

    fix: 멀티 채팅기능 구현(채팅방 포함)

--- src/main/java/client/TcpIpMultichatClient.java
+++ src/main/java/client/TcpIpMultichatClient.java
@@ -1,2 +1,97 @@
-package main.java.client;public class TcpIpMultichatClient {
-}
+package com.chat.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class TcpIpMultichatClient {
+
+    public static void main(String[] args) {
+        String name;
+        String roomId;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("닉네임을 입력해주세요.");
+        name = scanner.nextLine();
+
+        System.out.println("방 번호를 입력해주세요.");
+        roomId = scanner.nextLine();
+
+        try {
+            String serverIp = "127.0.0.1";
+            // 소켓을 생성하여 연결을 요청한다.
+            Socket socket = new Socket(serverIp, 7777);
+            System.out.println("서버에 연결되었습니다.");
+
+            Thread sender = new Thread(new ClientSender(socket, name, roomId));
+            Thread receiver = new Thread(new ClientReceiver(socket));
+
+            sender.start();
+            receiver.start();
+        } catch(ConnectException ce) {
+            ce.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    } // main
+
+    static class ClientSender extends Thread {
+        Socket socket;
+        DataOutputStream out;
+        String joinMessage;
+        String name;
+
+        ClientSender(Socket socket, String name, String roomId) {
+            this.socket = socket;
+            try {
+                out = new DataOutputStream(socket.getOutputStream());
+                this.name = name;
+                this.joinMessage = getJoinMessage(name, roomId);
+            } catch (Exception e) {
+            }
+        }
+
+        private String getJoinMessage(String name, String roomId) {
+            return "JOIN:" + roomId + ":" + name;
+        }
+
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                if (out != null) {
+                    out.writeUTF(joinMessage);
+                }
+
+                while (out != null) {
+                    out.writeUTF("[" + name + "]" + scanner.nextLine());
+                }
+            } catch (IOException e) {
+            }
+        } // run()
+    } // ClientSender
+
+    static class ClientReceiver extends Thread {
+        Socket socket;
+        DataInputStream in;
+
+        ClientReceiver(Socket socket) {
+            this.socket = socket;
+            try {
+                in = new DataInputStream(socket.getInputStream());
+            } catch (IOException e) {
+            }
+        }
+
+        public void run() {
+            while (in != null) {
+                try {
+                    System.out.println(in.readUTF());
+                } catch (IOException e) {
+                }
+            }
+        } // run
+    } // ClientReceiver
+} // class
--- src/main/java/server/TcpIpMultichatServer.java
+++ src/main/java/server/TcpIpMultichatServer.java
@@ -1,2 +1,115 @@
-package main.java.server;public class TcpIpMultichatServer {
-}
+package _ch16;
+
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class TcpIpMultichatServer {
+
+    Map<String, Map<String, DataOutputStream>> rooms;
+
+    TcpIpMultichatServer() {
+        rooms = Collections.synchronizedMap(new HashMap<String, Map<String, DataOutputStream>>());
+    }
+
+    public void start() {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        try {
+            serverSocket = new ServerSocket(7777);
+            System.out.println("서버가 시작되었습니다.");
+
+            while(true) {
+                socket = serverSocket.accept();
+                ServerReceiver thread = new ServerReceiver(socket);
+                thread.start();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void sendToRoomClients(String roomId, String msg) {
+        Map<String, DataOutputStream> roomClients = rooms.get(roomId);
+
+        if(roomClients != null) {
+            for (DataOutputStream out : roomClients.values()) {
+                try {
+                    out.writeUTF(msg);
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }// sendToAll
+
+    public static void main(String args[]) {
+        new TcpIpMultichatServer().start();
+    }
+
+    class ServerReceiver extends Thread {
+        Socket socket;
+        DataInputStream in;
+        DataOutputStream out;
+
+        ServerReceiver(Socket socket) {
+            this.socket = socket;
+            try {
+                in = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
+            } catch(IOException e) {}
+        }
+
+        public void run() {
+            String name = "";
+            String roomId = "";
+
+            try {
+                String joinMessage = in.readUTF();
+                String[] split = joinMessage.split(":");
+                roomId = split[1];
+                name = split[2];
+
+                synchronized (rooms) {
+                    if (!rooms.containsKey(roomId)) {
+                        rooms.put(roomId, Collections.synchronizedMap(new HashMap<>()));
+                        System.out.println("새 방 " + roomId + "가 생성되었습니다.");
+                    }
+                    rooms.get(roomId).put(name, out);
+                }
+
+                System.out.println("방 [" + roomId + "]의 사용자 수는 " + rooms.get(roomId).size() + "입니다.");
+
+                while(in != null) {
+                    String msg = in.readUTF();
+                    Map<String, DataOutputStream> roomClients = rooms.get(roomId);
+                    for(DataOutputStream out : roomClients.values()) {
+                        out.writeUTF(msg);
+                    }
+                }
+            } catch(IOException e) {
+                // Ignore
+            } finally {
+                if (rooms.containsKey(roomId)) {
+                    rooms.get(roomId).remove(name);
+                    if (rooms.get(roomId).isEmpty()) {
+                        rooms.remove(roomId); // 방이 비었다면 방도 제거
+                    }
+                }
+                // 같은 방의 클라이언트들에게 사용자가 나갔음을 알림
+                sendToRoomClients(roomId, "#" + name + "님이 나가셨습니다.");
+                System.out.println("[" + socket.getInetAddress() + " : " + socket.getPort() + "] 에서 접속을 종료하셨습니다.");
+                System.out.println("현재 [" + roomId + "] 방의 사용자 수는 " + rooms.get(roomId).size() + "입니다.");
+            } // try
+        } // run
+
+    } // ReceiverThread
+} // class
