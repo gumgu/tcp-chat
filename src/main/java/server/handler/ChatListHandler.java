f2a4b6e59df22c3070a08fd9572a634475e1955b
bien <87007010+gumgu@users.noreply.github.com>
2024-03-24 12:53:56 +0900

    feat: 채팅방(RoomManager) 기능 구현 (#4)
    - (Map rooms) Map 기반 채팅방 자료구조 생성
    - (RoomManager) 채팅방 리스트, 생성, 추가 기능 구현
    - Dispatcher에 RoomManager 인스턴스 생성하여 공통 사용하도록 구현
    - Chat 관련 핸들러에 채팅방 인스턴스 변수 설정

--- src/main/java/server/handler/ChatListHandler.java
+++ src/main/java/server/handler/ChatListHandler.java
@@ -1,6 +1,15 @@
 package main.java.server.handler;
 
+import main.java.server.manager.RoomManager;
+
 public class ChatListHandler implements MyHandlerAdapter {
+
+    private RoomManager roomManager;
+
+    public ChatListHandler(RoomManager roomManager) {
+        this.roomManager = roomManager;
+    }
+
     @Override
     public boolean support(Object handler) {
         return false;
