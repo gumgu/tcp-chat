929cb2fe7345aa08f1337d674afee6ab70dad834
bien <87007010+gumgu@users.noreply.github.com>
2024-04-03 00:13:56 +0900

    feat(Client-Handler): 클라이언트 연결 전략패턴으로 구현 (#9)
    - TcpIpMultichatClient: 클라이언트 측 연결 전략 패턴(strategy pattern)으로 구현
       - Handler로 단일 요청(getContent())과 다중 요청(maintainConnection()) 처리

---
+++ src/main/java/client/handler/FileUploadHandler.java
@@ -0,0 +1,10 @@
+package main.java.client.handler;
+
+import java.util.Scanner;
+
+public class FileUploadHandler implements Handler {
+    @Override
+    public String getContent(Scanner scanner) {
+        return null;
+    }
+}
